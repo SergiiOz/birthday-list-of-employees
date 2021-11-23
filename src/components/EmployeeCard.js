e185ed1a21fe8dcf18490b011d24002952df297a
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2021-11-23 23:39:46 +0200

    added radio buttons to EmployeeCard, but ui checked doesn't work right - will fix later

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -1,10 +1,49 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const EmployeeCard = ({ employee }) => {
+  const [status, setStatus] = useState('false');
+
+  const onChangeStatus = (event) => {
+    setStatus(event.currentTarget.value);
+  };
+
   return (
-    <li>
-      {employee.firstName} {employee.lastName}
-    </li>
+    <div>
+      <h4 className={status === 'true' ? 'active' : ''}>
+        {employee.firstName} {employee.lastName}
+      </h4>
+      {/* RADIO BUTTONS */}
+      {/* doesn't work ui checked radio button- need press doubleClick */}
+      <div>
+        <div>
+          <label>
+            <input
+              type="radio"
+              value="false"
+              name="status"
+              checked={status === 'false'}
+              onChange={onChangeStatus}
+            />{' '}
+            Not Active
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <input
+              type="radio"
+              value="true"
+              name="status"
+              checked={status === 'true'}
+              onChange={onChangeStatus}
+            />{' '}
+            Active
+          </label>
+        </div>
+      </div>
+      {status}
+    </div>
   );
 };
 
