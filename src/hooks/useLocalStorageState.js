d736ac03103d82632c2dd046b10efa63d2deca1c
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2021-12-01 00:15:25 +0200

    added hook useLocalSorage

---
+++ src/hooks/useLocalStorageState.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+//custom Hook
+const useLocalStorageState = (key, defaultValue = '') => {
+  const [state, setState] = useState(() => {
+    //lazy initialName
+    const valueInLocalStorage = window.localStorage.getItem(key);
+    if (valueInLocalStorage) {
+      return JSON.parse(valueInLocalStorage);
+    }
+    return defaultValue;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+};
+q;
